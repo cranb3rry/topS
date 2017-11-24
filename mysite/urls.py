a043c2a0d9746dbcd927cca2b7431b8fd5aea81a
cranb3rry <chimery@gmail.com>
2017-11-25 01:02:21 +0300

    win1

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^toptv/', include('toptv.urls')),
+    url(r'^topr/', include('topr.urls')),
 	url(r'^', include('home.urls')),
 	url(r'^', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
--- topr/views.py
+++ topr/views.py
@@ -1,3 +1,74 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+
+def index(request):
+    return HttpResponse('''
+<!DOCTYPE html>
+<html>
+<head>
+<title>HLS Stream</title>
+<style>
+    body {
+       
+    }
+</style>
+</head>
+<body>
+
+</script>
+<audio controls autoplay id="myaudio"><source src="http://noeight.net:888/top7.ogg" type="application/ogg" /></audio>
+
+<script>
+  var audio = document.getElementById("myaudio");
+  audio.volume = 0.3;
+</script>
+
+<p id="radioadmin"></p>
+<p id="radioartist"></p>
+<p id="radiosong"></p>
+
+<script>
+
+var myRadioadmin = document.getElementById("radioadmin");
+var myRadioartist = document.getElementById("radioartist");
+var myRadiosong = document.getElementById("radiosong");
+var myRadioartistPr = ''
+var myRadiosongPr = '' 
+
+
+setInterval(function() {
+fetch('http://noeight.net:888/status-json.xsl')
+.then((resp) => resp.json())
+.then(function(json) {
+  var stats = json.icestats
+  var radio = stats.source
+
+
+  if (radio.title != myRadiosongPr) {
+    myRadiosong.innerHTML = radio.title;
+    myRadiosongPr = radio.title;
+    
+  }
+
+  if (radio.artist != myRadioartistPr) {
+    myRadioartist.innerHTML = radio.artist;
+    myRadioartistPr = radio.artist;
+    
+  }
+
+
+    myRadioadmin.innerHTML = '';
+    
+    
+    })  
+}, 100);
+
+
+  </script>
+
+</body>
+</html>''')
