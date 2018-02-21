15713dc072b7ee137dc0e6badcc7ddb2d2c9f02b
cranb3rry <nine@noeight.net>
2018-02-21 17:35:03 +0000

    cpp1

---
+++ cpp/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iterator>
+
+using namespace std;
+
+int main () {
+
+    int number[] = { -5, 10, 15 };
+    cout << size(number) << '\n';
+    cout << number[1] << '\n';
+    return 0;
+}
---
+++ cpp/input.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <string>
+
+
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    std::string name1, phone1, address1;
+    std::string name2, phone2, address2;
+    
+    std::cout<<"user1, What is your name? \n";
+    std::getline(std::cin, name1);
+    std::cout<<"user1, What is your address? \n";
+    std::getline(std::cin, address1);
+    std::cout<<"user1, What is your phone? \n";
+    std::getline(std::cin, phone1);
+    
+    std::cout<<"user2, What is your name? \n";
+    std::getline(std::cin, name2);
+    std::cout<<"user2, What is your address? \n";
+    std::getline(std::cin, address2);
+    std::cout<<"user2, What is your phone? \n";
+    std::getline(std::cin, phone2);
+    
+    std::cout<<"\n"<<name1<<"\n\t\t"<<address1
+       <<"\n\t\t"<<phone1<<"\n";
+
+    std::cout<<"\n"<<name2<<"\n\t\t"<<address2
+       <<"\n\t\t"<<phone2<<"\n";
+
+    return 0;
+}
